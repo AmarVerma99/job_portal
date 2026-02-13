b706dc37d923480c31d5693bad823d84ecf150ae
AmarVerma99 <amarv0302@gmail.com>
2026-02-13 16:36:07 +0530

    frontend connected to render backend

--- frontend/utils/constant.js
+++ frontend/utils/constant.js
@@ -1,3 +1,2 @@
-export const User_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-
+export const User_API_END_POINT="https://job-portal-9l4q.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-9l4q.onrender.com/api/v1/job";
